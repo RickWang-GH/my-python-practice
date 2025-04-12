8e0a1b181a5c1c3a77d5484a1a87e0336f408a26
RickWang-GH <rickwang8895@gmail.com>
2025-04-12 22:32:54 +0800

    Initial commit of testv1.py

---
+++ testv1.py
@@ -0,0 +1,13 @@
+#V3.0.0
+
+def greet(name, enthusiasm=1):
+punctuation = "!" * enthusiasm
+message = f"Greetings, {name}{punctuation}" # 更新的問候
+print(message.upper()) # 轉換為大寫
+def main():
+user = "Alice" # 更改使用者
+greet(user, enthusiasm=3) # 增加熱情度
+if name == "main":
+# 添加一些註解xxxx222222
+main()
+
